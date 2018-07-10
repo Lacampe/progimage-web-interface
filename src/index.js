818c7eccde4dc45d1f96db47251065a599984100
Romain Campech <romain.campech@city.ac.uk>
2018-07-10 13:06:26 +0100

    React app rewired + antd + react router set up

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { App } from './components'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(
+  <Router>
+    <Route component={App} />
+  </Router>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
